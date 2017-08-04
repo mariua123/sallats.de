b7d31940f515cf7ea023876718ef2dc288cfbb88
Mario Sallat <msallat@stud.hs-offenburg.de>
2017-08-04 16:15:50 +0200

    Videos eingebunden, Logo eingebunden, Navi implementiert

--- js/main.js
+++ js/main.js
@@ -2,5 +2,30 @@
  * Created by mariosallat on 03.08.17.
  */
 jQuery(document).ready(function($) {
+    // $('.bxslider').bxSlider({
+    //     video: true,
+    //     useCSS: false
+    // });
+
+    var $slider = $('.bxslider').bxSlider({
+        // pager: false,
+        auto: true,
+        video: true,
+        // useCSS: false,
+        autoControls: false,
+        mode: 'horizontal',
+        pager: false,
+        speed: 1500,
+        pause: 5000,
+        preventDefaultSwipeY: false,
+        easing: 'ease-out',
+        infiniteLoop: true
+    });
+
+    $('.bx-prev, .bx-next').click(function () {
+        $slider.stopAuto();
+        // slider.startAuto();
+    });
+
 
 });
